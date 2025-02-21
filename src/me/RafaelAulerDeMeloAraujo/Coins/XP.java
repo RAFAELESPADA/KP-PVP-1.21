d6f8235eac0be60d71f40e91ab7c793f912817a1
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2025-02-21 10:10:45 -0300

    Update XP.java

--- src/me/RafaelAulerDeMeloAraujo/Coins/XP.java
+++ src/me/RafaelAulerDeMeloAraujo/Coins/XP.java
@@ -3,11 +3,12 @@ package me.RafaelAulerDeMeloAraujo.Coins;
 
 import java.util.HashMap;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import me.RafaelAulerDeMeloAraujo.main.Main;
 import net.wavemc.core.bukkit.WaveBukkit;
 import net.wavemc.core.bukkit.account.WavePlayer;
-
 
 public class XP
 {
@@ -24,8 +25,18 @@ public static HashMap<Player, Integer> bal = new HashMap();
 	  Sun8oxData.getPvp().addXP(amount);
   }
   
+  public static void setXP(Player player, int amount)
+  {
+	  WavePlayer Sun8oxData = WaveBukkit.getInstance().getPlayerManager().getPlayer(player.getName());
+	  Sun8oxData.getPvp().setXp(amount);
+  }
+  
   public static int getXP(Player player)
   {
+	  if (player == null) {
+		  Bukkit.getConsoleSender().sendMessage("Could not get a player XP");
+		  return 0;
+	  }
 	  WavePlayer Sun8oxData = WaveBukkit.getInstance().getPlayerManager().getPlayer(player.getName());
 	  
 	  if (Sun8oxData == null) {
@@ -37,12 +48,10 @@ public static HashMap<Player, Integer> bal = new HashMap();
   public static void removeXP(Player player, int amount)
   {
 	  WavePlayer Sun8oxData = WaveBukkit.getInstance().getPlayerManager().getPlayer(player.getName());
-	  Sun8oxData.getPvp().setXp(getXP(player) - amount);
+	  if (amount < Sun8oxData.getPvp().getCoins()) {
+	  setXP(player, Sun8oxData.getPvp().getXp() - amount);
+	  } else {
+		Sun8oxData.getPvp().setXp(Main.customization.getInt("MinimiumAmountOfXP"));  
+	  }
   }
-
-public static void setXP(Player player, int amount)
-{
-	  WavePlayer Sun8oxData = WaveBukkit.getInstance().getPlayerManager().getPlayer(player.getName());
-	  Sun8oxData.getPvp().setXp(amount);
-}
 }
